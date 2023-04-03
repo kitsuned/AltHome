292c31e5ab96a91dbd21e263e97a64a85d5960f6
kitsuned <80879390+kitsuned@users.noreply.github.com>
2023-04-03 16:40:07 +0500

    feat(luna): auto elevate on deny

--- src/shared/services/luna/model/luna.service.ts
+++ src/shared/services/luna/model/luna.service.ts
@@ -36,22 +36,52 @@ class LunaOneShot<T extends Record<string, any>, P extends LunaRequestParams = {
 
 	public constructor(public readonly uri: string, public readonly params?: P) {}
 
-	public call<T extends any>() {
+	public call() {
 		return new Promise<T>((resolve, reject) => {
 			this.bridge.onservicecallback = message => {
 				const parsed = JSON.parse(message);
 
+				console.log('<--', this.uri, parsed);
+
 				if (parsed.errorCode || !parsed.returnValue) {
+					if (typeof parsed?.errorText === 'string' && parsed.errorText.startsWith('Denied method call')) {
+						this.requestElevation().catch();
+					}
+
 					reject(parsed);
 				}
 
 				resolve(parsed);
 			};
 
+			console.log('-->', this.uri, this.params);
+
 			this.bridge.call(this.uri, JSON.stringify(this.params ?? {}));
 		});
 	}
+
+	private async requestElevation() {
+		const x = await luna<{ root: boolean; }>('luna://org.webosbrew.hbchannel.service/getConfiguration');
+
+		if (!x.root) {
+			await luna('luna://com.webos.notification/createToast', {
+				message: '[AltHome] Check root status!',
+			});
+
+			return;
+		}
+
+		await luna('luna://com.webos.notification/createToast', {
+			message: '[AltHome] Getting things ready…',
+		});
+
+		await luna('luna://org.webosbrew.hbchannel.service/exec', {
+			command: '/media/developer/apps/usr/palm/applications/com.kitsuned.althome/service --self-elevation',
+		});
+
+		window.close();
+	}
 }
 
-export const luna = <T extends Record<string, any>, P extends LunaRequestParams = {}, R extends LunaMessage = LunaMessage>(uri: string, params?: P) =>
-	new LunaOneShot<T, P>(uri, params).call<R>();
+export const luna = <T extends Record<string, any>, P extends LunaRequestParams = {}>(uri: string, params?: P) =>
+	new LunaOneShot<T, P>(uri, params).call();
