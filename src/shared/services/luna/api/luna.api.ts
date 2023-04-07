1d3f8e54dcee829e593fadf86be1dbf6f5159de3
kitsuned <80879390+kitsuned@users.noreply.github.com>
2023-04-08 04:43:02 +0500

    chore: better Luna errors handling

--- src/shared/services/luna/api/luna.api.ts
+++ src/shared/services/luna/api/luna.api.ts
@@ -1,6 +1,12 @@
-export type LunaMessage<T extends Record<string, any> = {}> = T & {
-	returnValue: boolean;
+export type LunaErrorMessage = {
+	returnValue: false;
+	errorCode?: number;
+	errorText?: string;
 };
+
+export type LunaMessage<T extends Record<string, any> = {}> = LunaErrorMessage | (T & {
+	returnValue: true;
+});
 
 export type LunaRequestParams<T extends Record<string, any> = Record<string, any>> = T & {
 	subscribe?: boolean;
