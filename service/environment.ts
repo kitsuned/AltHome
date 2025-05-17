792ca31db82e44c85315e36c6cd159a472494d2f
kitsuned <80879390+kitsuned@users.noreply.github.com>
2025-05-18 04:35:53 +0500

    refactor(service): extract constant

--- service/environment.ts
+++ service/environment.ts
@@ -7,3 +7,4 @@ export const APP_ID = process.env.APP_ID;
 export const SERVICE_ID = process.env.SERVICE_ID;
 export const SERVICE_ROOT_DIR = process.cwd();
 export const APP_ROOT_DIR = join(SERVICE_ROOT_DIR, `../../applications/${APP_ID}`);
+export const CANONICAL_HOME_APP_ID = 'com.webos.app.home';
