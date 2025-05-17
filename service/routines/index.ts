c8ee5ee9e7c92100908469a0cc803d663ceeaa64
kitsuned <80879390+kitsuned@users.noreply.github.com>
2025-05-18 04:42:32 +0500

    feat(service): add preload routine

--- service/routines/index.ts
+++ service/routines/index.ts
@@ -3,10 +3,16 @@ import type { Routine } from '../routine';
 
 import { KeyfilterRoutine } from './keyfilter.routine';
 import { MemoryManagerRoutine } from './memory-manager.routine';
+import { PreloadManagerRoutine } from './preload.routine';
 import { RootSymlRoutine } from './root-syml.routine';
 
 type RoutineCtor = {
 	new (service: Service): Routine;
 };
 
-export const routines: RoutineCtor[] = [RootSymlRoutine, KeyfilterRoutine, MemoryManagerRoutine];
+export const routines: RoutineCtor[] = [
+	RootSymlRoutine,
+	KeyfilterRoutine,
+	MemoryManagerRoutine,
+	PreloadManagerRoutine,
+];
