74f3b6052e4a92ee1cde498c589ba47b4f1d452a
kitsuned <80879390+kitsuned@users.noreply.github.com>
2026-02-01 05:20:33 +0500

    fix(settings): remove configd bindings, store settings in localStorage

--- packages/app/src/shared/services/settings/model/settings.service.ts
+++ packages/app/src/shared/services/settings/model/settings.service.ts
@@ -1,74 +1,35 @@
-import { comparer, makeAutoObservable, reaction, toJS, when } from 'mobx';
+import { comparer, makeAutoObservable, reaction, toJS } from 'mobx';
 
 import { injectable } from 'inversify';
 
-import { throttle } from '@althome/utils';
+const KEY = 'althome:settings';
 
-import { luna, LunaTopic } from '../../luna';
-
-const KEY = process.env.APP_ID as 'com.kitsuned.althome';
-
-type ConfigMessage = {
-	configs: {
-		[KEY]: Settings;
-	};
-	missingConfigs: string[];
-};
-
-type Settings = Omit<SettingsService, 'hydrated'>;
+type Settings = SettingsService;
 
 @injectable()
 export class SettingsService {
-	public hydrated: boolean = false;
-
 	public memoryQuirks: boolean = true;
 	public wheelVelocityFactor: number = 1.5;
 	public addNewApps: boolean = true;
 	public order: string[] = [];
 
-	private topic = new LunaTopic<ConfigMessage>('luna://com.webos.service.config/getConfigs', {
-		configNames: [KEY],
-		subscribe: true,
-	});
-
 	public constructor() {
-		this.saveConfig = throttle(this.saveConfig, 5 * 1000);
+		this.hydrate(JSON.parse(localStorage.getItem(KEY) ?? '{}'));
 
 		makeAutoObservable(this, {}, { autoBind: true });
 
-		reaction(
-			() => this.topic.message?.configs?.[KEY],
-			settings => this.hydrate(settings ?? {}),
-		);
-
-		when(
-			() => Boolean(this.topic.message?.missingConfigs),
-			() => this.hydrate({}),
-		);
-
-		when(
-			() => this.hydrated,
-			() => reaction(() => this.serialized, this.saveConfig, { equals: comparer.structural }),
-		);
+		reaction(() => this.serialized, this.saveConfig, { equals: comparer.structural });
 	}
 
 	private saveConfig(serialized: Settings) {
-		void luna('luna://com.webos.service.config/setConfigs', {
-			configs: {
-				[KEY]: serialized,
-			},
-		});
+		localStorage.setItem(KEY, JSON.stringify(serialized));
 	}
 
 	private get serialized(): Settings {
-		const { topic, hydrated, ...settings } = toJS(this);
-
-		return settings;
+		return toJS(this);
 	}
 
 	private hydrate(json: Partial<Settings>) {
-		this.hydrated = true;
-
 		Object.assign(this, json);
 	}
 }
